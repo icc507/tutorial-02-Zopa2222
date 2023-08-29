78579776a2cd216a34897ce7cf59b9a4223e0cfe
Sofia Medina <128067861+Zopa2222@users.noreply.github.com>
2023-08-29 08:45:33 -0400

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -9,6 +9,7 @@
 #		  mundo 44
 #La salida debe ser
 #         ('mundo', 44, 20, 90, 'hola', 'mundo', 44)
-t = input()
-m = input()
-print(m)
+t1 = input().split()
+t2 = input().split()
+
+print(t2+t1+t2)
