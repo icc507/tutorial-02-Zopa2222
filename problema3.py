9a47019c61c3ed285b13bdb6d83944cd088049bd
Sofia Medina <128067861+Zopa2222@users.noreply.github.com>
2023-08-29 09:59:17 -0400

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -9,5 +9,28 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+def arbolTrinario(numero):
+    return [int(numero), [], [], []]
+
+
+def insertaEnArbolTrinario(arbol, numero):
+    if arbol == []:
+        arbol += arbolTrinario(numero)
+    elif numero < arbol[0]:
+        insertaEnArbolTrinario(arbol[1], numero)
+    elif numero > arbol[0]:
+        insertaEnArbolTrinario(arbol[3], numero)
+    else:
+        if not isinstance(arbol[2], list):
+            arbol[2] = [arbol[2]]
+        arbol[2].append(numero)
+
+
+t = input().split()
+w = arbolTrinario(t[0])
+
+for i in range(1, len(t)):
+    insertaEnArbolTrinario(w, int(t[i]))
+
+print(w)
+
